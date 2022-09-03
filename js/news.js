27f73cb1a37df8d52d3a3a21d477e6d1e451535f
Mehedi2000 <mh4054528@gmail.com>
2022-09-03 19:11:22 +0600

    added category news

--- js/news.js
+++ js/news.js
@@ -6,16 +6,69 @@ const loadCategory = () => {
 }
 
 const displayCategory = (categorys) => {
-    console.log(categorys);
+    // console.log(categorys);
     const categoryContainer = document.getElementById('category-container');
     categorys.forEach(category => {
-        console.log(category);
+        // console.log(category)
         const categoryDiv = document.createElement('div')
-        // categoryDiv.classList.add( fs-4'fw-semibold')
         categoryDiv.innerHTML = `
-        <a class="fs-5 fw-semibold">${category.category_name}</a>
+        <a class="fs-5 fw-semibold" onclick="loadCategoryNews('${category.category_id}')">${category.category_name}</a>
         `;
         categoryContainer.appendChild(categoryDiv);
     });
 }
+
+const loadCategoryNews = (categoryId) => {
+    // console.log(categoryId)
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategoryNews(data.data))
+}
+
+const displayCategoryNews = (newsAll) => {
+    // console.log(newsAll)
+    const newsContainer = document.getElementById('news-container')
+    newsContainer.innerHTML = '';
+    newsAll.forEach(news => {
+        console.log(news)
+        const newsDiv = document.createElement('div')
+        newsDiv.classList.add('card', 'mb-5')
+        newsDiv.innerHTML = `
+        <div class="row g-0">
+            <div class="col-md-3">
+                <img src="${news.thumbnail_url}" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+            <div class="col-md-9">
+                <div class="card-body">
+                    <h5 class="card-title fs-5 fw-bold my-3">${news.title}</h5>
+                    <p class="card-text">${news.details.slice(0, 300)}</p>
+
+                    <div class="mt-4 d-flex justify-content-between fw-semibold">
+
+                    <div class"d-flex justify-content-between">
+                    <img src="${news.author.img}" style="height: 80px;" class="rounded-circle">
+                    <span>
+                    <p>Name : ${news.author.name ? news.author.name : 'Not found'}</p>
+                    <p>Date : ${news.author.published_date ? news.author.published_date : 'Not found'}</p>
+                    </span>
+                    </div>
+
+                    <div>
+                    <p>view : ${news.total_view ? news.total_view : 'Not found'}</p>
+                    </div>
+
+                    <div>
+                    <button class="btn btn-primary fw-bold px-4 py-2">Details</button>
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+            `;
+        newsContainer.appendChild(newsDiv)
+    });
+}
+
 loadCategory();
